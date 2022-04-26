e1a7d88c9c047ba41e9747a91bde2a44dff0d63a
asafworld <gabrielasaf10@gmail.com>
2022-04-26 19:43:54 -0300

    Resolve problemas de lint no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
-  render() { 
+  render() {
     return (
       <section>
-          <Link
-            to="/cart"
-            data-testid="shopping-cart-button"
-          >
-            Cart
-          </Link>
+        <Link
+          to="/cart"
+          data-testid="shopping-cart-button"
+        >
+          Cart
+        </Link>
       </section>
     );
   }
 }
- 
+
 export default Header;
