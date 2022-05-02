3fbe398a1aeb04af1845157131f1f18939fba92f
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-05-01 22:57:39 -0300

    Primeira versão de css Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import priceFormat from '../services/priceFormat';
+import './Cart.css';
 
 class Cart extends Component {
   constructor() {
     super();
     this.state = {
       products: [],
+      totalPrice: 0,
     };
   }
 
   componentDidMount = () => {
     const itemsId = localStorage.getItem('addedProductsIds');
-    const ids = JSON.parse(itemsId);
-    this.setState({ products: ids });
+    const products = JSON.parse(itemsId);
+    this.setState({ products });
+    let total = 0;
+    products.forEach((e) => {
+      total += (e.price * e.qntd);
+    });
+    this.setState({ totalPrice: total });
   }
 
   plusOne = (item) => {
@@ -26,6 +33,11 @@ class Cart extends Component {
     });
     this.setState({ products });
     localStorage.setItem('addedProductsIds', JSON.stringify(products));
+    let total = 0;
+    products.forEach((e) => {
+      total += (e.price * e.qntd);
+    });
+    this.setState({ totalPrice: total });
   }
 
   minusOne = (item) => {
@@ -40,6 +52,11 @@ class Cart extends Component {
       this.setState({ products });
       localStorage.setItem('addedProductsIds', JSON.stringify(products));
     }
+    let total = 0;
+    products.forEach((e) => {
+      total += (e.price * e.qntd);
+    });
+    this.setState({ totalPrice: total });
   }
 
   remove = (item) => {
@@ -47,6 +64,11 @@ class Cart extends Component {
     const newArray = products.filter((e) => e.id !== item.id);
     this.setState({ products: newArray });
     localStorage.setItem('addedProductsIds', JSON.stringify(newArray));
+    let total = 0;
+    newArray.forEach((e) => {
+      total += (e.price * e.qntd);
+    });
+    this.setState({ totalPrice: total });
   }
 
   exist = (item) => {
@@ -60,7 +82,7 @@ class Cart extends Component {
   }
 
   render() {
-    const { products } = this.state;
+    const { products, totalPrice } = this.state;
     return (
       <>
         <header>
@@ -72,37 +94,85 @@ class Cart extends Component {
         </header>
         { products.length === 0
           ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p> : (
-            products.map((item, index) => (
-              <div key={ index }>
-                <p data-testid="shopping-cart-product-name">{ item.title }</p>
-                <img src={ item.thumbnail } alt={ item.title } />
-                <p>{ priceFormat((item.price * item.qntd)) }</p>
-                <p data-testid="shopping-cart-product-quantity">
-                  { item.qntd }
-                </p>
-                <button
-                  type="button"
-                  onClick={ () => this.plusOne(item) }
-                  data-testid="product-increase-quantity"
-                  disabled={ this.exist(item) }
+            products.map((item) => (
+              <section key={ item.id } className="card-container">
+                <section
+                  data-testid="product"
+                  className="card"
                 >
-                  +
-                </button>
-                <button
-                  type="button"
-                  onClick={ () => this.minusOne(item) }
-                  data-testid="product-decrease-quantity"
-                  disabled={ item.qntd === 1 }
-                >
-                  -
-                </button>
-                <button type="button" onClick={ () => this.remove(item) }>x</button>
-              </div>
+                  <img className="img-card" src={ item.thumbnail } alt={ item.title } />
+                  <div className="product-info">
+                    <Link to={ `/product/${item.id}` }>
+                      <p
+                        data-testid="shopping-cart-product-name"
+                        className="name-card"
+                      >
+                        { item.title }
+                      </p>
+                    </Link>
+                    <p
+                      className="price-card"
+                    >
+                      { priceFormat((item.price * item.qntd))}
+                    </p>
+                    <p
+                      className="stock-card"
+                    >
+                      { `Estoque: ${item.available_quantity}` }
+                    </p>
+                    { item.shipping.free_shipping && (
+                      <p data-testid="free-shipping" className="free">
+                        <i className="fa-solid fa-truck-fast" />
+                        {' Frete Grátis'}
+                      </p>
+                    ) }
+                    <div className="control-cart">
+                      <button
+                        type="button"
+                        onClick={ () => this.minusOne(item) }
+                        data-testid="product-decrease-quantity"
+                        disabled={ item.qntd === 1 }
+                        className="more-less-cart less"
+                      >
+                        -
+                      </button>
+                      <p data-testid="shopping-cart-product-quantity">{ item.qntd }</p>
+                      <button
+                        type="button"
+                        onClick={ () => this.plusOne(item) }
+                        data-testid="product-increase-quantity"
+                        disabled={ this.exist(item) }
+                        className="more-less-cart more"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    onClick={ () => this.remove(item) }
+                    className="delete-cart"
+                  >
+                    ⨉
+                  </button>
+                </section>
+              </section>
             ))
           ) }
-        <Link to="/checkout" data-testid="checkout-products">
-          Finalizar Compra
-        </Link>
+        <footer className="footer-cart">
+          <p className="total">
+            Total:
+            {' '}
+            <span className="total-price">{ priceFormat(totalPrice) }</span>
+          </p>
+          <Link
+            to="/checkout"
+            data-testid="checkout-products"
+            className="checkout-link"
+          >
+            Finalizar Compra
+          </Link>
+        </footer>
       </>
     );
   }
